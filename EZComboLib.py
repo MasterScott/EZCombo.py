d1958cffe6369fcd42777c2788c84f62d91c5f67
Pure | Cody <43043067+PurityWasHere@users.noreply.github.com>
2020-06-10 17:01:47 -0700

    Update EZComboLib.py
    Added the ability to Save Good / Bad Combos.
    Added the ability to Capture Data with Combo! Use GoodCombo(capture , data) ex: example1:pass1 | Total Bits: 0
    Added TotalCombo Var instead of only using ComboIterator. You can now tell the program when checking is done by checking if ComboIterator == TotalCombo
    Fixed File close bug from saving hits.

--- EZComboLib.py
+++ EZComboLib.py
@@ -1,21 +1,22 @@
 ComboList = []
+GoodCombos = []
+BadCombos = []
+TotalCombos = 0
 ComboIterator = 0
 Username = ''
 Password = ''
 Combo = ''
-GoodCombos = []
 
 def LoadCombos(ComboPath):
     global ComboList
-    global ComboIterator
+    global TotalCombos
     try:
         Combos = open(ComboPath,'r')
         for line in Combos:
             strippedcombo = line.replace('\n','')
             ComboList.append(strippedcombo)
-            ComboIterator+=1
-        return ComboList
-        return ComboIterator
+            TotalCombos+=1
+        return ComboList , TotalCombos
     except IOError:
         print('Could Not Find ComboList. Is the Path Correct?')
 
@@ -25,8 +26,7 @@ def PullCombo(ComboIterator):
     global ComboList
     try:
         Username,Password = ComboList[ComboIterator].split(':',1)
-        return Username
-        return Password
+        return Username , Password
     except:
         print('Error Parsing Combo. Is it in proper Format? (Username:Password) Maybe you forgot to call LoadCombos() first')
 
@@ -44,6 +44,36 @@ def SaveValids(SaveName):
         ValidFile = open(SaveName,'a')
         for line in GoodCombos:
             ValidFile.write(line + "\n")
+        ValidFile.close()
         return GoodCombos
     except:
         print('Error Saving Good Combos!')
+
+def GoodCombo(capture , data):
+    try:
+        global Username
+        global Password
+        global GoodCombos
+        GoodCombos.append(Username + ':' + Password + capture + data)
+    except:
+        print("Error Appending Combo. Did you pass Capture Name and Capture Data?")
+
+def BadCombo():
+    try:
+        global Username
+        global Password
+        global BadCombos
+        BadCombos.append(Username + ':' + Password)
+    except:
+        print("Error Appending Bad Combo")
+
+def SaveFails(SaveName):
+    global BadCombos
+    try:
+        BadFile = open(SaveName,'a')
+        for line in BadCombos:
+            BadFile.write(line + "\n")
+        BadFile.close()
+        return BadCombos
+    except:
+        print('Error Saving Bad Combos!')
